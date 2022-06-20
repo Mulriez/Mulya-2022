f8575f44f0271991bb3f9dafa614c200fd643177
Mulriez <pesonarizqi17@gmail.com>
2022-06-20 12:00:30 +0700

    mul

--- js/it.js
+++ js/it.js
@@ -1,10 +1,13 @@
 var nama = document.getElementById('nama')
 var email = document.getElementById('email')
 var pesan = document.getElementById('pesan')
+var form = document.getElementById('cs')
 
 var nama_error = document.getElementById('nama_error')
 var email_error = document.getElementById('email_error')
 var pesan_error = document.getElementById('pesan_error')
+
+var errorData = true;
 
 email.addEventListener('textInput', emailVerify);
 nama.addEventListener('textInput', namaVerify);
@@ -14,20 +17,20 @@ function validated() {
     if (email.value.length < 9) {
         email.style.border = '1px solid red';
         email_error.style.display = 'block';
-        email.focus();
-        
+        errorData = false; 
     }
     if (nama.value.length < 4) {
         nama.style.border = '1px solid red';
         nama_error.style.display = 'block';
-        nama.focus();
-        
+        errorData = false;
     }
     if (pesan.value.length < 4) {
         pesan.style.border = '1px solid red';
         pesan_error.style.display = 'block';
-        pesan.focus();
-        
+        errorData = false;
+    }
+    if (errorData == false) {
+        return false;
     }
 }
 
@@ -35,20 +38,20 @@ function emailVerify() {
     if (email.value.length >= 9) {
         email.style.border = '1px solid silver';
         email_error.style.display = 'none';
-        return true;
+        errorData = true;
     }
 }
 function namaVerify() {
     if (nama.value.length >= 4) {
         nama.style.border = '1px solid silver';
         nama_error.style.display = 'none';
-        return true;
+        errorData = true;
     }
 }
 function pesanVerify() {
     if (pesan.value.length >= 4) {
         pesan.style.border = '1px solid silver';
         pesan_error.style.display = 'none';
-        return true;
+        errorData = true;
     }
 }
